f16236777ccea39c692286d37540db3f76420a7d
BuildTools <unconfigured@null.spigotmc.org>
2020-02-04 08:37:30 +0100

    Client now fetches JSOn object from server

--- skilt-info/screens/DisplayInformationScreen.js
+++ skilt-info/screens/DisplayInformationScreen.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const DisplayInformationScreen = props => {
+    const [data, setData] = useState("")
+    const [hasErrors, setHasErrors] = useState(false);
+
+
+    async function fetchData(){
+        const res = await fetch("http://24d0f727.ngrok.io/?id=85404247");
+        res
+        .json()
+        .then(res => setData(res))
+        .catch(err => setHasErrors(true));
+    }
+
+    useEffect(() => {
+        fetchData();
+    })
+
+
+    
+    getData = () => {
+        console.log(data);
+    }
+
     return(
         <View style={styles.container}>
             <Text>This is the DisplayInformationScreen!</Text>
+            <Button title="Get data" onPress={() => getData()}></Button>
         </View>
     );
 };
