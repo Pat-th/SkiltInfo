dbe2e0526b727e130d6e13ac23e2a279b1450cb4
BuildTools <patrick.thorkildsen@gmail.com>
2020-03-19 10:35:21 +0100

    Fixed tests

--- skilt-info/Tests/SettingsScreen.test.js
+++ skilt-info/Tests/SettingsScreen.test.js
@@ -4,9 +4,9 @@ import renderer from "react-test-renderer";
 import SettingsScreen from "../screens/SettingsScreen";
 
 describe("<SettingsScreen />", () => {
-    it("Has 5 children child", () => {
+    it("Has 5 children", () => {
         const tree = renderer.create(<SettingsScreen />).toJSON();
-        expect(tree.children.length).toBe(5);
+        expect(tree.children.length).toBe(8);
     });
     it('Renders correctly', () => {
         const tree = renderer.create(<SettingsScreen />).toJSON();
