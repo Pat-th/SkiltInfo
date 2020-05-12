4c66522dbd901a582eeadc3fb5b831ecc7f12ff0
BuildTools <patrick.thorkildsen@gmail.com>
2020-05-12 10:45:40 +0200

    Pulled master and fixed tests

--- skilt-info/Tests/CreateNewFilterScree.test.js
+++ skilt-info/Tests/CreateNewFilterScree.test.js
@@ -4,9 +4,9 @@ import renderer from "react-test-renderer";
 import CreateNewFilterScreen from "../screens/CreateNewFilterScreen";
 
 describe("<CreateNewFilterScreen />", () => {
-    it("Has 1 child", () => {
+    it("Has 2 child", () => {
         const tree = renderer.create(<CreateNewFilterScreen />).toJSON();
-        expect(tree.children.length).toBe(1);
+        expect(tree.children.length).toBe(2);
     });
     it('Renders correctly', () => {
         const tree = renderer.create(<CreateNewFilterScreen />).toJSON();
