712ad237f5167dd2d82d41bc54287a10974933a9
Quanern <quan_7_nt@hotmail.com>
2020-05-14 14:53:33 +0200

    Added generated javadoc and jacoco

---
+++ Java/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CreateUriUtil"},{"p":"<Unnamed>","l":"HttpRoadSignDao"},{"p":"<Unnamed>","l":"Server"},{"p":"<Unnamed>","l":"WGS2UTM"}]
